285b7784613428e83c37b90327f7a25ed524dac6
Domenico Mastrangelo <gdmastrangelo@gmail.com>
2021-02-01 23:15:34 +0100

    Added database provision tests

--- database/database_test.go
+++ database/database_test.go
@@ -1,13 +1,65 @@
 package database
 
 import (
+	"database/sql"
 	"testing"
 )
 
-func TestCheckDatabaseConnection(t *testing.T) {
-	db := Check()
+func TestCheck(t *testing.T) {
+	var db interface{}
 
-	if db == nil {
+	db = Check()
+
+	if _, ok := db.(*sql.DB); !ok {
 		t.FailNow()
 	}
+}
+
+func TestCheckDatabaseConnection(t *testing.T) {
+	var db interface{}
+
+	db = Check()
+
+	if _, ok := db.(*sql.DB); !ok {
+		t.FailNow()
+	}
+}
+
+func TestCheckDataTable(t *testing.T) {
+	db := Check()
+
+	var check interface{}
+	check = checkDataTable(db)
+
+	if _, ok := check.(bool); !ok {
+		t.FailNow()
+	}
+}
+
+func TestCheckTypesTable(t *testing.T) {
+	db := Check()
+
+	var check interface{}
+	check = checkTypesTable(db)
+
+	if _, ok := check.(bool); !ok {
+		t.FailNow()
+	}
+}
+
+func TestCheckConfigTable(t *testing.T) {
+	db := Check()
+
+	var check interface{}
+	check = checkConfigTable(db)
+
+	if _, ok := check.(bool); !ok {
+		t.FailNow()
+	}
+}
+
+func TestCheckDatabaseProvision(t *testing.T) {
+	db := Check()
+
+	checkDatabaseProvision(db)
 }
